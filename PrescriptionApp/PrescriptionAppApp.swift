4c8a92364d4b7fa35022550a7de32fec88b07e39
escandiuzzi99x <luize@99x.io>
2023-11-20 11:47:47 -0300

    Initial Commit

---
+++ PrescriptionApp/PrescriptionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrescriptionAppApp.swift
+//  PrescriptionApp
+//
+//  Created by Luiz Felipe Escandiuzzi on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PrescriptionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
